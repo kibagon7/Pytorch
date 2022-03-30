988b726e8c8ccd73e9b3bd9ccca339f145986a4e
kibagon7 <kibagon.y.i@gmail.com>
2022-03-30 20:32:49 +0900

    t

--- __init__.py
+++ __init__.py
@@ -2,7 +2,11 @@
 # Apache License Version 2.0
 # https://www.apache.org/licenses/LICENSE-2.0.html
 
-README = "eval_loss(loader, device, net, criterion), fit(net, optimizer, criterion, num_epochs, train_loader, test_loader, device, history), evaluate_history(history), show_images_labels(loader, classes, net, device) "
+README = "
+eval_loss(loader, device, net, criterion), 
+fit(net, optimizer, criterion, num_epochs, train_loader, 
+test_loader, device, history), evaluate_history(history), 
+show_images_labels(loader, classes, net, device) "
 
 
 
